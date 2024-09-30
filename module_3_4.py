f21641a1d593f9e33a40b1fde796b8617ac7ca03
Sergey Sokhatskiy <132381396+kalina-7@users.noreply.github.com>
2024-09-30 22:36:22 +0300

    Update module_3_4.py

--- module_3_4.py
+++ module_3_4.py
@@ -3,7 +3,7 @@ def single_root_words(*other_words, root_word = 'rich'):
     for i in other_words:
         if root_word in i and i != root_word:
             same_words.append(i)
-    return(same_words)
+    return same_words
 
 
 result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
@@ -14,7 +14,7 @@ def single_root_words(*other_words, root_word = 'Disable'):
     for i in other_words:
         if i.lower() in root_word.lower():
             same_words.append(i)
-    return(same_words)
+    return same_words
 
 
 result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
