ad99bba89065ed0e31dd2cefd79b2e8dfd57935a
Sergey Sokhatskiy <132381396+kalina-7@users.noreply.github.com>
2024-10-10 18:51:14 +0300

    10.10

---
+++ fake_math.py
@@ -0,0 +1,8 @@
+def divide(first, second):
+    if second != 0:
+        return first / second
+    else:
+        return 'Ошибка'
+
+
+# print(divide(20, 0))
---
+++ true_math.py
@@ -0,0 +1,10 @@
+from math import inf
+
+def divide(first, second):
+    if second != 0:
+        return first / second
+    else:
+        return inf
+
+
+# print(divide(20, 5))
