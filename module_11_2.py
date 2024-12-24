debb320df64af63c69272e82054b112d2c9918aa
Sergey Sokhatskiy <132381396+kalina-7@users.noreply.github.com>
2024-12-24 21:11:15 +0300

    Merge remote-tracking branch 'github/main'

--- module_11_2.py
+++ module_11_2.py
@@ -3,7 +3,7 @@ import inspect
 def introspection_info(obj):
     return {
         'type': type(obj).__name__,
-        'attributes': dir(obj),
+        'attributes': [attribute for attribute in dir(obj) if not callable(getattr(obj, attribute))],
         'methods': [method for method in dir(obj) if callable(getattr(obj, method))],
         'module': inspect.getmodule(obj)
     }
